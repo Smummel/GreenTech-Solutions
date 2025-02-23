532c13b9efb1a3dace664a133722ce05f6f96dca
Samuel Basson <samuelbasson06@gmail.com>
2025-02-23 19:23:42 +0100

    added greentech

---
+++ script.js
@@ -0,0 +1,3 @@
+
+function tjansterScroll(){document.getElementById('tjanster').scrollIntoView({behavior: 'smooth'});}
+function bokaScroll(){document.getElementById('kontakt').scrollIntoView({behavior: 'smooth'});}
